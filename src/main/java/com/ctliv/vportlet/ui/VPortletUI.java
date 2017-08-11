1545f25555fb7dca6b6f2d8395e7629bd6b74209
ctoncelli <ct.livorno@gmail.com>
2017-08-11 21:58:09 +0200

    Working...

--- src/main/java/com/ctliv/vportlet/ui/VPortletUI.java
+++ src/main/java/com/ctliv/vportlet/ui/VPortletUI.java
@@ -1,18 +1,27 @@
 package com.ctliv.vportlet.ui;
 
+import javax.portlet.PortletContext;
 import javax.portlet.PortletMode;
+import javax.portlet.PortletSession;
 
 import org.osgi.service.component.annotations.Component;
 
 import com.ctliv.lvs.component.DebugLayout;
 import com.ctliv.lvs.spring.util.BeanUtil;
 import com.ctliv.lvs.ui.MultimodeUIExt;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.WrappedPortletSession;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Widgetset(AppWidgetSet.NAME)
@@ -31,7 +40,7 @@ public class VPortletUI extends MultimodeUIExt {
 
     @Override
     protected void init(VaadinRequest request) {
-    	/*
+
         final String portletContextName = getPortletContextName(request);
         final Integer numOfRegisteredUsers = getPortalCountOfRegisteredUsers();
         final VerticalLayout layout = new VerticalLayout();
@@ -52,33 +61,35 @@ public class VPortletUI extends MultimodeUIExt {
             }
         );
         layout.addComponent(button);
-        */
+        DebugLayout debugLayout = BeanUtil.getBean(DebugLayout.class);
+        log.debug("Created: " + debugLayout);
+        this.setComponent(PortletMode.EDIT, debugLayout);
     	
-    	this.setComponent(PortletMode.VIEW, BeanUtil.getBean(DebugLayout.class));
-    	this.setComponent(PortletMode.EDIT, BeanUtil.getBean(DebugLayout.class));
+//    	this.setComponent(PortletMode.VIEW, BeanUtil.getBean(DebugLayout.class));
+//    	this.setComponent(PortletMode.EDIT, BeanUtil.getBean(DebugLayout.class));
         log.debug("UI set");
     }
 
-//    private String getPortletContextName(VaadinRequest request) {
-//        WrappedPortletSession wrappedPortletSession = (WrappedPortletSession) request
-//                .getWrappedSession();
-//        PortletSession portletSession = wrappedPortletSession
-//                .getPortletSession();
-//        
-//        final PortletContext context = portletSession.getPortletContext();
-//        final String portletContextName = context.getPortletContextName();
-//        return portletContextName;
-//    }
-//
-//    private Integer getPortalCountOfRegisteredUsers() {
-//        Integer result = null;
-//
-//        try {
-//            result = UserLocalServiceUtil.getService().getUsersCount();
-//        } catch (SystemException e) {
-//            log.error(e);
-//        }
-//
-//        return result;
-//    }
+    private String getPortletContextName(VaadinRequest request) {
+        WrappedPortletSession wrappedPortletSession = (WrappedPortletSession) request
+                .getWrappedSession();
+        PortletSession portletSession = wrappedPortletSession
+                .getPortletSession();
+        
+        final PortletContext context = portletSession.getPortletContext();
+        final String portletContextName = context.getPortletContextName();
+        return portletContextName;
+    }
+
+    private Integer getPortalCountOfRegisteredUsers() {
+        Integer result = null;
+
+        try {
+            result = UserLocalServiceUtil.getService().getUsersCount();
+        } catch (SystemException e) {
+            log.error(e);
+        }
+
+        return result;
+    }
 }

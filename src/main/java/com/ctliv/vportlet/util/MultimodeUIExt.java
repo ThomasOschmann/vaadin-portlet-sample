8b78c32d3edb0a20e83302f47df0649c2ebc3ceb
ctoncelli <ct.livorno@gmail.com>
2017-09-03 14:21:47 +0200

    Working...

--- src/main/java/com/ctliv/vportlet/util/MultimodeUIExt.java
+++ src/main/java/com/ctliv/vportlet/util/MultimodeUIExt.java
@@ -8,19 +8,27 @@ import com.vaadin.server.VaadinPortletRequest;
 import com.vaadin.server.VaadinPortletService;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.VerticalLayout;
 
+@SuppressWarnings("serial")
 public abstract class MultimodeUIExt extends UIExt {
 
-	private static final long serialVersionUID = 1L;
-	
 	private static final PortletMode DEFAULT_MODE = PortletMode.VIEW;
 	
 	private HashMap<PortletMode,AbstractComponent> map = new HashMap<>();
-
+	
+	private VerticalLayout layout = new VerticalLayout();
+	private AbstractComponent header = null;
+	private AbstractComponent footer = null;
+	
 //	@Override
 //	protected void init(VaadinRequest request) {
-//		selectMode(JpaExWebUtil.getPortletMode());
+//		selectMode(WebUtil.getPortletMode());
 //	}
+	
+	public MultimodeUIExt() {
+		layout.setMargin(false);
+	}
 
 	@Override
 	protected void refresh(VaadinRequest request) {
@@ -30,10 +38,30 @@ public abstract class MultimodeUIExt extends UIExt {
 		super.refresh(request);
 	}	
 	
+	public AbstractComponent getHeader() {
+		return header;
+	}
+	
+	public void setHeader(AbstractComponent header) {
+		this.header = header;
+	}
+	
+	public AbstractComponent getFooter() {
+		return footer;
+	}
+	
+	public void setFooter(AbstractComponent footer) {
+		this.footer = footer;
+	}
+	
 	private void selectMode(PortletMode mode) {
 		AbstractComponent component = map.get(mode);
 		if (component == null) component = map.get(DEFAULT_MODE);
-		this.setContent(component);
+		layout.removeAllComponents();
+		if (header != null) layout.addComponent(header);
+		if (component != null) layout.addComponent(component);
+		if (footer != null) layout.addComponent(footer);
+		this.setContent(layout);
 	}
 	
 	public AbstractComponent getComponent(PortletMode mode) {

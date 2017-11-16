a4a8199c21e0fc8c3a0c4f5e2f05f8317e49ba4b
ctoncelli <ct.livorno@gmail.com>
2017-11-16 23:42:48 +0100

    Working...

--- src/main/java/com/github/ctliv/vportlet/export/AppWidgetSet.java
+++ src/main/java/com/github/ctliv/vportlet/export/AppWidgetSet.java
@@ -8,7 +8,7 @@ import com.vaadin.osgi.resources.OsgiVaadinWidgetset;
 public class AppWidgetSet implements OsgiVaadinWidgetset {
 
 	//This maps to file "src/main/resources/com/github/ctliv/vportlet/AppWidgetSet.gwt.xml";
-	public static final String NAME = "com.github.AppWidgetSet";
+	public static final String NAME = "AppWidgetSet";
 
 	@Override
     public String getName() {

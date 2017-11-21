f6641eb06e218170a79406b3c3ef48524ea8eeb1
ctliv <ct.livorno@gmail.com>
2017-11-21 19:13:24 +0100

    Working...

---
+++ src/main/webapp/VAADIN/widgetsets/com.ctliv.rainbow.AppWidgetSet/deferredjs/94224886DA99F588B07F0413085EFA07/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_ctliv_rainbow_AppWidgetSet.runAsyncCallback2('jfb(1852,1,w7d);_.Xb=function pqc(){H4b((!z4b&&(z4b=new P4b),z4b),this.a.d)};A1d(xh)(2);\n//# sourceURL=com.ctliv.rainbow.AppWidgetSet-2.js\n')

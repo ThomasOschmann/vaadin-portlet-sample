ceebda9bb61b990a859cb1791c21cba5c81d8c8a
ctliv <ct.livorno@gmail.com>
2017-11-22 18:23:49 +0100

    Working...

--- src/main/java/com/github/ctliv/vportlet/export/VPortletUI.java
+++ src/main/java/com/github/ctliv/vportlet/export/VPortletUI.java
@@ -38,7 +38,7 @@ public class VPortletUI extends UI {
 		
     }
 
-    @WebServlet(urlPatterns = "/*", name = "VPortletUIServlet", asyncSupported = true)
+    @WebServlet(urlPatterns = "/vportlet", name = "VPortletUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = VPortletUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
